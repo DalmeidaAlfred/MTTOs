515ca2c7cf3a2b98655ecb75f03f50d95b3c3a48
DavidAlmeida <dalmeida@alfredsmart.com>
2024-08-12 17:32:57 +0200

    pls

--- watchdog_signage/watchdog_signage.sh
+++ watchdog_signage/watchdog_signage.sh
@@ -18,7 +18,7 @@ while true; do
         done
         sleep 10
         #Start the signage application
-        sudo /home/pi/Downloads/optisigns-5.6.32-arm64.AppImage &
+        sudo /home/pi/Downloads/optisigns-5.6.32-arm64.AppImage
         sleep 10
         echo "$(date): Signage process started." >> "$LOG_FILE"
     else
