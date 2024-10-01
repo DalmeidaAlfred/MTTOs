20544ec1eef0ad15e22dfa6cbe5a877d9d141bf0
DavidAlmeida <dalmeida@alfredsmart.com>
2024-10-01 17:21:27 +0200

    added file for via augusta b

---
+++ utilidades/fix_via_augusta_b.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Define the file paths to the rules and configuration files
+RULES_FILE="/etc/openhab2/rules/community_Dahua.rules"
+DUMMIES_FILE="/home/openhabian/.alfredassistant/dummies.ini"
+
+# Backup the original rules and configuration files before making changes
+cp "$RULES_FILE" "${RULES_FILE}.bak"
+cp "$DUMMIES_FILE" "${DUMMIES_FILE}.bak"
+
+# Overwrite the dummies.ini file with the new content
+cat <<EOL > "$DUMMIES_FILE"
+[DUMMYComm201]
+default_name=Puerta B
+default_room=Piscina
+device_type=DUMMY_SWITCH
+default_usage=CommunityDoor
+
+[DUMMYComm202]
+default_name=Puerta B
+default_room=Terraza
+device_type=DUMMY_SWITCH
+default_usage=CommunityDoor
+
+[DUMMYComm102]
+default_name=Principal
+default_room=Comunidad
+device_type=DUMMY_SWITCH
+default_usage=CommunityDoor
+EOL
+
+cat <<EOL > "$RULES_FILE"
+rule "Open community door"
+when
+  Item ALFRED_DUMMY102_DUMMY_LOCK_DoorLock received command ON
+then
+  val USER="admin"
+  val PASSWORD="AlfredSmart"
+
+  executeCommandLine("/etc/openhab2/scripts/community_dahua.sh " + USER + " " + PASSWORD + " " + triggeringItem.name + " " + receivedCommand, 15000)  
+end
+rule "Open Pool B door"
+when
+  Item ALFRED_DUMMY201_DUMMY_LOCK_DoorLock received command ON
+then
+  val USER="0000000081b7f468"
+  val PASSWORD="C2tVM4khusCvLSBbSK1Cg4eMUhIo1z"
+
+  executeCommandLine("/etc/openhab2/scripts/community_door.sh " + USER + " " + PASSWORD + " " + triggeringItem.name + " " + receivedCommand, 15000)  
+end
+
+rule "Open Terraza door"
+when
+  Item ALFRED_DUMMY202_DUMMY_LOCK_DoorLock received command ON
+then
+  val USER="0000000081b7f468"
+  val PASSWORD="C2tVM4khusCvLSBbSK1Cg4eMUhIo1z"
+
+  executeCommandLine("/etc/openhab2/scripts/community_door.sh " + USER + " " + PASSWORD + " " + triggeringItem.name + " " + receivedCommand, 15000)  
+end
+EOL
+
+echo "Replacement completed. Original files backed up as ${RULES_FILE}.bak and ${DUMMIES_FILE}.bak"
+sudo systemctl restart alfred-assistant
