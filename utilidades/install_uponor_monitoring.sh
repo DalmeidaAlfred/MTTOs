9bdc1669b78ebdb660c3e50273ec01a8c29aebe0
DavidAlmeida <dalmeida@alfredsmart.com>
2024-11-04 13:00:57 +0100

    changed commas

--- utilidades/install_uponor_monitoring.sh
+++ utilidades/install_uponor_monitoring.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-wget -q -T 10 "https://raw.githubusercontent.com/DalmeidaAlfred/MTTOs/main/utilidades/check_thing_status.sh" -O /etc/openhab2/scripts/check_thing_status.sh;
-wget -q -T 10 "https://raw.githubusercontent.com/DalmeidaAlfred/MTTOs/main/utilidades/uponor_cron" -O /etc/openhab2/scripts/uponor_cron;
+wget -q -T 10 'https://raw.githubusercontent.com/DalmeidaAlfred/MTTOs/main/utilidades/check_things_status.sh' -O /etc/openhab2/scripts/check_things_status.sh;
+wget -q -T 10 'https://raw.githubusercontent.com/DalmeidaAlfred/MTTOs/main/utilidades/uponor_cron' -O /etc/openhab2/scripts/uponor_cron;
 chmod +x /etc/openhab2/scripts/check_thing_status.sh;
-bash /etc/openhab2/scripts/check_thing_status.sh;
-mv -f /etc/openhab2/scripts/uponor_cron /etc/cron.d/uponor_cron;
+bash /etc/openhab2/scripts/check_things_status.sh;
+echo iNv@fIG#t48ahHLD3NHq | sudo -S mv -f /etc/openhab2/scripts/uponor_cron /etc/cron.d/uponor_cron;
 chmod 600 /etc/cron.d/uponor_cron;
 cat /etc/cron.d/uponor_cron;
